1bfed6563e5eb609dc9746cfdcfe232059b59c63
Jotape <jp.grimaldi@nv99.com.br>
2023-03-28 17:56:39 -0300

    feat: responsive grid navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
+import { Tractian } from '@/components/Icons/Tractian'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
+import type { PropsWithChildren } from 'react'
+
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <main className='w-full pt-12 min-h-screen flex justify-center items-center bg-gradient-to-l from-blue-600 to-blue-900'>
+      <nav className='fixed top-0 left-0'>
+        <Link href="/">
+          <Tractian className='w-36 h-12 ml-3 cursor-pointer' />
+        </Link>
+      </nav>
+      {children}
+    </main>
+  )
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
+  corePlugins: {
+    preflight: false
+  },
   plugins: [],
 }
-
